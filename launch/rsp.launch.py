d23f50c89a3b697dd2184ab3e664df820c8c9bdd
omar55221 <omar91010@yahoo.com>
2024-09-19 18:41:14 +0300

    added ros2 controllers

--- launch/rsp.launch.py
+++ launch/rsp.launch.py
@@ -1,41 +1,41 @@
 import os
-
 from ament_index_python.packages import get_package_share_directory
-
 from launch import LaunchDescription
 from launch.substitutions import LaunchConfiguration
 from launch.actions import DeclareLaunchArgument
 from launch_ros.actions import Node
-
 import xacro
 
 
 def generate_launch_description():
-
-    # Check if we're told to use sim time
+    # Declare LaunchConfiguration for sim time
     use_sim_time = LaunchConfiguration('use_sim_time')
 
-    # Process the URDF file
+    # Process the URDF file using xacro
     pkg_path = os.path.join(get_package_share_directory('void_bot'))
-    xacro_file = os.path.join(pkg_path,'description','robot.urdf.xacro')
+    xacro_file = os.path.join(pkg_path, 'description', 'robot.urdf.xacro')
+
+    # Parse the xacro file into XML format (URDF)
     robot_description_config = xacro.process_file(xacro_file)
     
     # Create a robot_state_publisher node
-    params = {'robot_description': robot_description_config.toxml(), 'use_sim_time': use_sim_time}
     node_robot_state_publisher = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
         output='screen',
-        parameters=[params]
+        parameters=[{'robot_description': robot_description_config.toxml(),
+                     'use_sim_time': use_sim_time}]
     )
 
-
-    # Launch!
+    # Launch description
     return LaunchDescription([
+        # Declare the use_sim_time argument (default is false)
         DeclareLaunchArgument(
             'use_sim_time',
             default_value='false',
-            description='Use sim time if true'),
+            description='Use simulation time if true'
+        ),
 
+        # Add the robot_state_publisher node to the launch description
         node_robot_state_publisher
     ])
